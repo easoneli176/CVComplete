3de649fda95b973d9d9bc6e8da43b0ed1dad6024
Lizzie Eason <easoneli17@gmail.com>
2024-04-09 16:04:06 -0600

    initial package

---
+++ R/foldfield.R
@@ -0,0 +1,29 @@
+#' Adds a field to a dataframe which encodes stratified folds
+#'
+#' @param data the dataset
+#' @param numfolds the number of desired folds for cross validation
+#' @param stratify the name of the field in the dataset which we wish to use to stratify the cv folds
+#' @return a dataset
+#' @examples
+#' mock_data <- data.frame(preds = rep(1:4,10),target=rep(1:5,8),strat=rep(1:2,20))
+#' new data set:
+#' newdat<-fold_field(mock_data,3,"strat")
+#' check that stratification worked:
+#' table(newdat$fold,newdat$strat)
+
+fold_field<-function(data,numfolds,stratify){
+  stratnum<-which(colnames(data) == stratify)
+  strat<-data[,stratnum]
+
+  set.seed(123)
+  folds<-createFolds(strat, k=numfolds, list=TRUE, returnTrain=FALSE)
+
+  data$fold<-1
+
+  for (i in 1:dim(data)[1]){
+    for (j in 2:numfolds){
+      data$fold[i]<-ifelse(i %in% folds[[j]],j,data$fold[i])
+    }
+  }
+  data
+}
